7a1969dd944307af4504aee572c0f6bbfaeb4e78
cbselman <cbselman@gmail.com>
2022-07-19 06:11:48 -0500

    Added global variables for scores. game() resets score when run and returns an alert with final scores. playRound() now returns a point increase for round  winner.

--- rpsGame.js
+++ rpsGame.js
@@ -1,3 +1,7 @@
+let playerScore;
+let computerScore;
+let ties;
+
 function getComputerChoice() {
     
     let number = Math.floor(Math.random() * (4 - 1) + 1);
@@ -21,39 +25,53 @@ function playRound(playerSelection, computerSelection) {
     let playerWin = `You win! ${playerSelection} beats ${computerSelection}`;
 
     if (playerSelection === computerSelection) {
-        return alert("It's a tie!");
+        alert("It's a tie!");
+        return ++ties;
     }
     else if (playerSelection === 'ROCK') {
         if (computerSelection === 'PAPER') {
-            return alert(playerLose);
+            alert(playerLose);
+            return ++computerScore;
         } else if (computerSelection === 'SCISSORS') {
-            return alert(playerWin);
+            alert(playerWin);
+            return ++playerScore;
         }
     }
     else if (playerSelection === 'PAPER') {
         if (computerSelection === 'SCISSORS') {
-            return alert(playerLose);
+            alert(playerLose);
+            return ++computerScore;
         } else if (computerSelection === 'ROCK') {
-            return alert(playerWin);
+            alert(playerWin);
+            return ++playerScore;
         }
     }
     else if (playerSelection === 'SCISSORS') {
         if (computerSelection === 'ROCK') {
-            return alert(playerLose);
+            alert(playerLose);
+            return ++computerScore;
         }
         else if (computerSelection === 'PAPER') {
-            return alert(playerWin);
+            alert(playerWin);
+            return ++playerScore;
         }
     }
     else {
-        return alert("That is not a valid answer!")
+        alert("That is not a valid answer! Point for computer!");
+        return ++computerScore;
     }
 }
 
 function game() {
+    playerScore = 0;
+    computerScore = 0;
+    ties = 0;
+
     for (i = 0; i < 5; i++) {
         let playerSelection = prompt("Please enter your choice of rock, paper, or scissors:", "");
         let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+        playRound(playerSelection, computerSelection);
     }
+
+    return alert(`The final score is ${playerScore} to ${computerScore}, with ${ties} draws!`);
 }
